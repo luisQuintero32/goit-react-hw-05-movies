969299db48af890353877b1a2cd7b196290d5e30
luisQuintero32 <luisQuintero32@users.noreply.github.com>
2023-07-24 02:42:54 +0000

    deploy: 2c9889687906336a9f53c892a38854f6f7ea4bf1

---
+++ assets/Home-851bdcce.js
@@ -0,0 +1,1 @@
+import{r as s,j as e}from"./index-d345dd2c.js";import{n as i,M as n,a}from"./normalized-data-d1bf4c15.js";import{f as m}from"./api-d8b78cbe.js";import"./genres-names-2136eebc.js";function u(){const[t,o]=s.useState([]);return s.useEffect(()=>{m().then(r=>{o(i(r.results))})},[]),e.jsx(n,{children:e.jsx(a,{data:t})})}function v(){return e.jsx(u,{})}export{v as default};
